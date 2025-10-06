e67d4725b3a2b736e1d0c836779088a043efdd48
Alsainey Barry <ab22316053@utg.edu.gm>
2025-10-06 19:41:46 +0000

    initial commit

---
+++ src/main/java/com/Alsainey/Spring/Security/with/RBAC/SpringSecurityWithRbacApplication.java
@@ -0,0 +1,13 @@
+package com.Alsainey.Spring.Security.with.RBAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithRbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithRbacApplication.class, args);
+	}
+
+}
